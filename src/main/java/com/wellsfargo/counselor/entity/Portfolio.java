93d0f59be606834ea6c7df0115b9358ac594480c
Ojaswa Varshney <118374998+JrG-One@users.noreply.github.com>
2024-06-28 22:17:46 +0530

    Create Portfolio.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,14 @@
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio")
+    private List<Security> securities;
+}
