b39c19e6bc26ce6ac66ad722dd95833cddbf12af
Ojaswa Varshney <118374998+JrG-One@users.noreply.github.com>
2024-06-28 22:16:51 +0530

    Create FinancialAdvisor.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/FinancialAdvisor.java
@@ -0,0 +1,12 @@
+@Entity
+public class FinancialAdvisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "financialAdvisor")
+    private List<Client> clients;
+}
