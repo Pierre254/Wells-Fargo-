c0a8a5d3c0bc48baae700af8b998f2ca07af0859
harshavardhan <haarshavardhan08@gmail.com>
2024-07-06 00:52:33 -0500

    Using JPA created classes for each Entity in data model

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,45 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long portfolioId;
+
+    @ManyToOne
+    private Client clientId;
+
+    @Column(nullable = false)
+    private Date creationDate;
+
+
+    public Portfolio() {
+
+    }
+
+    public Portfolio(Client clientId, Date creationDate) {
+        this.clientId = clientId;
+        this.creationDate = creationDate;
+    }
+
+    public Client getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Client clientId) {
+        this.clientId = clientId;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
