d1ae4a47c670c8ecaae39dba16da31ea55d0f50c
scamitt <amitanand@amits-MacBook-Air.local>
2024-09-11 00:07:50 +0530

    Your commit message

---
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -0,0 +1,91 @@
+package com.wellsfargo.counselor.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolioId")
+    private Portfolio portfolio;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private long purchasePrice;
+
+    @Column(nullable = false)
+    private String purchaseDate;
+
+    @Column(nullable = false)
+    private long quantity;
+
+    protected Security() {
+
+    }
+
+    public Security(String name, String category, long purchasePrice, String purchaseDate, long quantity) {
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.purchaseDate = purchaseDate;
+        this.quantity = quantity;
+    }
+
+    public long getSecurityId() {
+        return securityId;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public long getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(long purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+}
